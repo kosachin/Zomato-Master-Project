12fac7c49d52377fad1aec23d8e823f0757c8aa8
sachin <kosachin313@gmail.com>
2021-08-04 18:22:05 +0530

    add signin route

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -9,37 +9,39 @@ import {UserModel} from "../../database/user";
 const Router = express.Router();
 
 /*
-Route       /signup
-des         /Signup with email and password
-Params      /none
-Access      Public
-Method      POST
+Route     /signup
+Des       Register new user
+Params    none
+Access    Public
+Method    POST  
 */
 Router.post("/signup", async (req, res) => {
-    try{        
-        await UserModel.findByEmailAndPhoneNumber(req.body.credentials);
-        
-        // save to DB
-        const newuser = await UserModel.create(req.body.credentials);
-        
-        // generate JWT auth token and
-        const token = newUser.generateJwtToken();
-        
-        // return
-        return res.status(200).json({token, status: "success"});
-        
-    }catch(error){
-        return res.status(500).json({error: error.message});
-    };
+  try {
+    await UserModel.findByEmailAndPhone(req.body.credentials);
+    const newUser = await UserModel.create(req.body.credentials);
+    const token = newUser.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 /*
-Route       /signin
-des         /Signup with email and password
-Params      /none
-Access      Public
-Method      POST
+Route     /signin
+Des       Signin with email and password
+Params    none
+Access    Public
+Method    POST  
 */
+Router.post("/signin", async (req, res) => {
+  try {
+    const user = await UserModel.findByEmailAndPassword(req.body.credentials);
 
+    const token = user.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 export default Router; 
--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,32 +1,19 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
-const UserSchema = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true
+const UserSchema = new mongoose.Schema(
+    {
+      fullname: { type: String, required: true },
+      email: { type: String, required: true },
+      password: { type: String },
+      address: [{ detail: { type: String }, for: { type: String } }],
+      phoneNumber: [{ type: Number }],
     },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String
-    },
-    address: [{
-        detail: {
-            type: String
-        },
-        for: {
-            type: String
-        }
-    }],
-    phoneNumber: [{
-        type: Number
-    }],
-}, {
-    timestamps: true,
-})
+    {
+      timestamps: true,
+    }
+  );
 
 UserSchema.methods.generateJwtToken = function () {
     return jwt.sign({user: this._id.toString() }, "ZomatoAPP");
@@ -48,6 +35,24 @@ UserSchema.statics.findByEmailAndPhoneNumber = async ({email, phoneNumber }) => 
     }
 
     return false;
+};
+
+UserSchema.statics.findByEmailAndPassword = async ({email, password }) => {
+
+    // Check whether email exist
+    const user = await UserModel.findOne({email});
+    if (!user) {
+        throw new Error("User does not Exist!!!");
+    }
+    
+    // Compare password
+    const doesPasswordMatch = await bcrypt.compare(password,user.password);
+        
+    if (!doesPasswordMatch) {        
+        throw new Error("Invalid password!!!");
+    }
+
+    return user;
 };
 
 UserSchema.pre("save", function (next) {
