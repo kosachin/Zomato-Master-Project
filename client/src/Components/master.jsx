a269998157aa6a9de6ac6dd8396b2eb78273de7e
sachin <kosachin313@gmail.com>
2021-08-24 16:13:56 +0530

    add mobile and pc screen foodtab

---
+++ client/src/Components/master.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+// Components
+// import Delivery from "./Delivery";
+// import Dining from "./Dining";
+// import NightLife from "./NightLife";
+// import Nutrition from "./Nutrition";
+
+const Master = () => {
+  const { type } = useParams();
+  return (
+    <div className="my-5">
+
+    </div>
+  );
+};
+
+export default Master;
--- client/src/Layout/Home.layout.jsx
+++ client/src/Layout/Home.layout.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 // Components 
 import Navbar from '../Components/Navbar';
+import FoodTab from '../Components/FoodTab';
+
 
 
 const HomeLayout = (props) => {
     return (
         <>
         <div className="container mx-auto px-4 lg:px-20 "><Navbar />{props.children}</div>
+        <FoodTab />
         </>
     );
 
