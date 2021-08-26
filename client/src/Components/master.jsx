d905dc6de34c15c71296ad74184bf7edaa360c8c
sachin <kosachin313@gmail.com>
2021-08-26 12:01:31 +0530

    add delivery section

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 // Components
-// import Delivery from "./Delivery";
+import Delivery from "./Delivery";
 // import Dining from "./Dining";
 // import NightLife from "./NightLife";
 // import Nutrition from "./Nutrition";
@@ -11,6 +11,7 @@ const Master = () => {
   const { type } = useParams();
   return (
     <div className="my-5">
+    {type === "delivery" && <Delivery />}
 
     </div>
   );
