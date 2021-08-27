8930c0af28b9a464d5ef0d8769e38dc94ecab43e
sachin <kosachin313@gmail.com>
2021-08-27 16:07:54 +0530

    add food category carous

--- client/src/Components/Delivery/DeliveryCategory.jsx
+++ client/src/Components/Delivery/DeliveryCategory.jsx
@@ -22,7 +22,7 @@ const DeliverySmCard = ({ image, title }) => {
 const DeliveryLgCard = ({ image, title }) => {
   return (
     <>
-      <div className="hidden lg:block w-64 h-48">
+      <div className="hidden lg:block w-full h-48 px-4">
         <div className="w-full h-full">
           <img
             src={image}
